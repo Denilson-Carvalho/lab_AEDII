5f8e96fbb21d7f4058c3de8603189ecfb9ada762
Denilson-Carvalho <denilsongabrielfc@gmail.com>
2023-08-12 11:15:14 -0300

    tudo feito

--- questao3.c
+++ questao3.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(){
+void findPythagoreanTriples(int limit) {
+    for (int a = 1; a <= limit; a++) {
+        for (int b = a; b <= limit; b++) {
+            for (int c = b; c <= limit; c++) {
+                if (a * a + b * b == c * c) {
+                    printf("Cateto 1: %d, Cateto 2: %d, Hipotenusa: %d\n", a, b, c);
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    int limit;
+    printf("Encontre todos os triplos pitagóricos com até qual valor? ");
+    scanf("%d", &limit);
+
+    printf("Triplos pitagóricos para catetos e hipotenusa até %d:\n", limit);
+    findPythagoreanTriples(limit);
 
     return 0;
 }
