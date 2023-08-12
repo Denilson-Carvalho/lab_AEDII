4a455cb085b281228a5d0a8685d8336d75ed93c4
Denilson-Carvalho <denilsongabrielfc@gmail.com>
2023-08-11 22:11:46 -0300

    7 ok

--- questao7.c
+++ questao7.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
 
+int f(int m, int n){
+    if(n==1){
+        return m + 1;
+    }else if (m == 1){
+        return n + 1;
+    }else if( m > 1 || n > 1){
+        return f(m, n-1) + f(m-1, n );
+    }
+    
+}
+
+
+int main(){
+    int n, m;
+    n = 10;
+    m = 2;
+    f(m, n);
+    
     return 0;
 }
